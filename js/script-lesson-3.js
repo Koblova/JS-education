5b02e57455176a9ec26235986692e60a0ba1b172
Koblova <alina.kirov@mail.ru>
2022-03-31 23:57:23 +0300

    lesson 3 homeworks task 7

--- js/script-lesson-3.js
+++ js/script-lesson-3.js
@@ -63,3 +63,91 @@ b1.onclick = () => {
 }
 
 //Даны две переменные a21 и a22. При нажатии кнопки b-2, запускается функция f2. Функция должна сравнить переменные с помощью if else и вывести в out-2 число, которое больше. Вариант равенства переменных не рассматриваем.
+
+let a21 = document.querySelector(".a-21");
+let a22 = document.querySelector(".a-22");
+let b2 = document.querySelector(".b-2");
+let out2 = document.querySelector(".out-2");
+
+b2.onclick = () => {
+  let f2 = +a21.value;
+  let f33 = +a22.value;
+  if (f2 >= f33); {
+    out2.innerHTML = f2;
+  }
+  if (f2 <= f33) {
+    out2.innerHTML = f33;
+  }
+}
+
+//Даны 2 input - i-31 и i-32, оба - input[type=number]. При нажатии кнопки b-3 срабатывает функция f3. Функция должна прочитать содержимое i-31 и i-32 в переменные и сравнить их, вывести в out-3 большее число.
+
+//Проведите самостоятельный тест работы, введите пары чисел 4 и 9, 9 и 22, 5 и 111.
+
+let i31 = document.querySelector(".i-31");
+let i32 = document.querySelector(".i-32");
+let b3 = document.querySelector(".b-3");
+let out3 = document.querySelector(".out-3");
+
+b3.onclick = () => {
+  let f3 = +i31.value;
+  let f44 = +i32.value;
+  if (f3 > f44) {
+    out3.innerHTML = f3;
+  }
+  else if (f44 > f3) {
+    out3.innerHTML = f44;
+  }
+}
+
+//Создайте на странице input[type=number] с классом i-4, куда пользователь может ввести год своего рождения. Есть кнопка b-4 которая запускает функцию f4. Функция должна вывести в .out-4 ok если пользователю больше или равно 18 лет, и Oh если меньше.
+
+let b4 = document.querySelector(".b-4");
+let i4 = document.querySelector(".i-4");
+let out4 = document.querySelector(".out-4");
+
+b4.onclick = () => {
+  let f4 = +i4.value;
+  if (2022 - f4 > 18) {
+    out4.innerHTML = "ok!";
+  }
+  else if (2022 - f4 < 18) {
+    out4.innerHTML = "you are baby!";
+  }  
+}
+
+// Создайте на странице input[type=number] с классом i-5, куда пользователь может ввести число. Есть кнопка b-5 которая запускает функцию f5. Функция должна вывести в .out-5 символ m если число меньше нуля, 0 если число равно нулю и 1 если больше.
+
+let b5 = document.querySelector(".b-5");
+let i5 = document.querySelector(".i-5");
+let out5 = document.querySelector(".out-5");
+
+b5.onclick = () => {
+  let f5 = +i5.value;
+  if (f5 < 0) {
+    out5.innerHTML = "m"
+  }
+  else if (f5 == 0) {
+    out5.innerHTML = "0"
+  }
+  if (f5 > 0) {
+    out5.innerHTML = "1"
+  }
+}
+
+//Создайте на странице input[type=number] с классом i-6, куда пользователь может ввести число. Есть кнопка b-6 которая запускает функцию f6. Функция должна вывести в .out-6 слово even если число четное и odd если нечетное. Для проверки четности используется целочисленный остаток от деления на 2 (оператор %). Если остаток равен нулю - четное, нет - нечетное.
+
+let b6 = document.querySelector(".b-6");
+let i6 = document.querySelector(".i-6");
+let out6 = document.querySelector(".out-6");
+
+b6.onclick = () => {
+  let f6 = +i6.value % 2;
+  if (f6 == 0) {
+    console.log("odd");
+  }
+  if (f6 != 0) {
+    console.log("even");
+  }
+}
+
